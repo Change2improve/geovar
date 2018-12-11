f568abd3469fca8bfa53459277d3cf52ed3f6d1f
moe93 <m.odeh93@gmail.com>
2018-12-11 14:48:15 -0500

    Complete working version

--- onshape.py
+++ onshape.py
@@ -2,15 +2,19 @@
 *
 * Configure Onshpae parts using Python
 *
-* VERSION: 1.0
-*   - ADDED   : Initial release
+* VERSION: 1.1
+*   - ADDED   : Script is now COMPLETELY independent of hardcoded
+*               values. Script can now determine number of
+*               features and dynamically adjust as needed.
 *
 * KNOWN ISSUES:
-*   - Nada so far.
+*   - No speed optimizations have been attempted yet. For now, script
+*     communicates EVERY feature variable to Onshape, even though that
+*     variable has not changed from the last iteration.
 * 
 * AUTHOR                    :   Mohammad Odeh
 * DATE                      :   Dec. 10th, 2018 Year of Our Lord
-* LAST CONTRIBUTION DATE    :               -----
+* LAST CONTRIBUTION DATE    :   Dec. 11th, 2018 Year of Our Lord
 *
 '''
 
@@ -20,25 +24,51 @@ from    platform                    import  system                  # Running pl
 from    datetime                    import  datetime                # Get date and time
 from    pexpect                     import  spawn                   # Call external programs
 from    argparse                    import  ArgumentParser          # Add input arguments to script
+from    itertools                   import  product                 # Apply product rule on combinations
+import  numpy                       as      np                      # Fast array creation
 import  os, re                                                      # Dir/path manipulation, extract numerics from strings
 
 # ************************************************************************
 # =====================> CONSTRUCT ARGUMENT PARSER <=====================*
 # ************************************************************************
+
 ap = ArgumentParser()
 
-ap.add_argument( "--dev-mode", action = 'store_true',
-                 dest = "dev_mode", default= False  ,
-                 help = "Enter developer mode"      )
+ap.add_argument( "--dev-mode"           ,
+                 dest   = "dev_mode"    ,
+                 action = 'store_true'  , default=False ,
+                 help   = "Enter developer mode"        )
 
-ap.add_argument( "--tetgen-dir"     , type = str    ,
-                 dest = "tetgen_dir", default="foo" ,
-                 help = "Point to TetGen directory" )
+ap.add_argument( "--tetgen-dir"         , type = str    ,
+                 dest   = "tetgen_dir"  , default="foo" ,
+                 help   = "Point to TetGen directory"   )
+
+ap.add_argument( "-v", "--verbose"      ,
+                 dest   = "verbose"     ,
+                 action = 'store_true'  , default=False ,
+                 help   = "WARNING: Prints EVERYTHING!!")
+
+ap.add_argument( "-LB", "--lower-bound" , type = int    ,
+                 dest   = "lower_bound" , default = 50  ,
+                 help   = "Minimum value desired"       )
+
+ap.add_argument( "-UB", "--upper-bound" , type = int    ,
+                 dest   = "upper_bound" , default = 51  ,
+                 help   = "Maximum value desired"       )
+
+ap.add_argument( "-H", "--step-size"    , type = float  ,
+                 dest   = "upper_bound" , default = 0.1 ,
+                 help   = "Variations step size"        )
 
 args = ap.parse_args()
 
 args.dev_mode    = True
-args.tetgen_dir  = '/home/moe/Desktop/geovar/tetgen1.5.1/'
+if( args.dev_mode ):
+    args.tetgen_dir     = '/home/moe/Desktop/geovar/tetgen1.5.1/'
+    args.verbose        = True
+    args.lower_bound    = 9
+    args.upper_bound    = 10
+    args.step_size      = 0.5
 # ************************************************************************
 # ===========================> PROGRAM  SETUP <==========================*
 # ************************************************************************
@@ -53,7 +83,6 @@ class GeoVar( object ):
         self.connect_to_sketch()                                    # Instantiate Onshape client and connect
 
         self.get_default()                                          # Get configurable part features and CURRENT default values
-        self.mutate_part()                                          # Do the thing
 
 # --------------------------
 
@@ -150,47 +179,47 @@ class GeoVar( object ):
         
 # --------------------------
 
-    def mutate_part( self ):
+    def mutate_part( self, arr ):
         '''
         Apply product rule on part to get as many
         geometric variations as needed
 
-        NOTE:-
+        INPUT:-
+            - arr:  An array of arrays containing the values
+                    we would our features to have
+                    
+        NOTES:-
             You MUST multiply the value with whatever unit
             you want it to be (i.e 3*u.in == 3in)
         '''
-        LB = 0; UB = 2                                              # Define lower and upper bounds
-        for i in range( LB, UB ):
+        
+        ranges  = [range(arr.shape[1])] * arr.shape[0]
+        b       = np.array( list(product(*ranges)) )
+        
+        for i in range( 0, b.shape[0] ):
+            
             for ii in range( 0, len(self.keys) ):
                 print( "{}".format( self.keys[ii] ), end='\t\t' )
             print( "t_regen" )
-            print( "========================================================\n" )
-            scalar_1 = (i+1)/float(UB)
-            
-            for j in range( LB, UB ):
-                scalar_2 = (j+1)/float(UB)
-                
-                for k in range( LB, UB ):
-                    scalar_3 = (k+1)/float(UB)
+            print( "========================================================" )
 
-                    start   = time()
-                    self.myPart.params = { 'r_inner' : ( 50*scalar_1)*u.mm,
-                                           'r_outer' : (100*scalar_2)*u.mm,
-                                           'height'  : (100*scalar_3)*u.mm }
-                    end     =  time() - start
-                    
-                    print( "{:4.3f} \t\t {:4.3f} \t {:4.3f} \t {:4.3f}".format(50*scalar_1,
-                                                                               100*scalar_2,
-                                                                               100*scalar_3, end) )
-                    print( "--------------------------------------------------------" )
-                    
-                    # get the STL export
-                    self.export_stl( i, j, k )
-                    
+            temp    = str()
+            start   = time()
+            for j in range( 0, arr.shape[0] ):
+                self.myPart.params = { self.keys[j]: arr.T[b[i][j]][j]*u.mm }
+                print( "{:4.3f}".format( arr.T[b[i][j]][j] ), end='\t\t' )
+
+                temp = "{}{}{}__".format( temp, self.keys[j], arr.T[b[i][j]][j] )
+            print( "{:4.3f}".format(time() - start) )
+            print( "--------------------------------------------------------\n" )
+
+            # get the STL export
+            file = "{}{}.stl".format( self.dst, temp.rstrip('_') )
+            self.export_stl( file )
 
 # --------------------------
 
-    def export_stl( self, i, j, k ):
+    def export_stl( self, file_name ):
         '''
         Apply product rule on part to get as many
         geometric variations as needed
@@ -202,7 +231,6 @@ class GeoVar( object ):
 
         stl = self.c.part_studio_stl( self.did, self.wid, self.eid )
 
-        file_name = "{}ri{}_ro{}_h{}.stl".format( self.dst, i, j, k )
         with open( file_name, 'w' ) as f:
             f.write( stl.text )
 
@@ -210,11 +238,23 @@ class GeoVar( object ):
         child = spawn(cmd, timeout=None)                            # Spawn child
         for line in child:                                          # Read STDOUT ...
             out = line.decode('unicode-escape').strip('\r\n')       # ... of spawned child ...
-            print( out )                                            # ... process and print.
+            if( args.verbose ): print( out )                        # ... process and print.
         child.close()                                               # Kill child process
 
 # ************************************************************************
 # =========================> MAKE IT ALL HAPPEN <=========================
 # ************************************************************************
 
-GeoVar()
+prog = GeoVar()                                                     # Startup and prepare program
+
+''' CHANGE THESE GUYS AS YOU SEE FIT '''
+LB  = args.lower_bound                                              # Lower bound
+UB  = args.upper_bound                                              # Upper bound
+h   = args.step_size                                                # Step size
+
+arr = np.zeros( [len(prog.keys), int((UB-LB)/h)+1] )                # Dynamically create array
+for i in range( 0, len(prog.keys) ):                                # depending on number of
+    arr[i] = np.array( np.linspace(LB, UB, (UB-LB)/h+1) )           # varying features
+
+prog.mutate_part( arr )                                             # Do da tang!
+
