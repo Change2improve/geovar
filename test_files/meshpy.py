ce61cebb5e197a8866fc624a164c7bae35266c6c
moe93 <m.odeh93@gmail.com>
2019-01-04 14:19:58 -0500

    Detect boundary edges

--- test_files/meshpy.py
+++ test_files/meshpy.py
@@ -1,40 +1,75 @@
+'''
+*
+* Find edge boundaries by the examining normal vectors' direction
+*
+* VERSION: 1.1
+*   - ADDED   : Initial release
+*   - FIXED   : Sorta work how it is intended
+*
+* KNOWN ISSUES:
+*   - In the test case of the cylinder, the top and
+*     bottom lids are not detected.
+*
+*
+* AUTHOR                    :   Mohammad Odeh
+* DATE                      :   Dec. 21st, 2018 Year of Our Lord
+* LAST CONTRIBUTION DATE    :   Jan.  4th, 2019 Year of Our Lord
+*
+'''
+
 import  pymesh
-from    stl                     import  mesh                as  meh
 import  numpy                                               as  np
-##from    mayavi                import  mlab                            # Data visualization
 from    matplotlib              import  pyplot              as plt
 from    mpl_toolkits.mplot3d    import  Axes3D
 from    matplotlib.patches      import  FancyArrowPatch
 from    mpl_toolkits.mplot3d    import  proj3d
+
 # Define path
-dd      = "/home/moe/Desktop/test_files/r_inner1.0__r_outer10.0__height100.1.mesh"
-mesh    = pymesh.load_mesh( dd )                            # Load mesh
+dd      = "/home/moe/Desktop/test_files/r_inner9.5__r_outer10.0__height9.1.mesh"
+mesh    = pymesh.load_mesh( dd )                                    # Load mesh
 
-mesh.add_attribute("face_normal")                           # Compute normals
-normals = mesh.get_attribute("face_normal")                 # Retrieve them
+mesh.add_attribute("face_normal")                                   # Compute normals
+normals = mesh.get_attribute("face_normal")                         # Retrieve them
 
-mesh.add_attribute("face_index")                            # Compute Indices
-index = mesh.get_attribute("face_index")                    # Retrieve them
+mesh.add_attribute("face_index")                                    # Compute Indices
+index = mesh.get_attribute("face_index")                            # Retrieve them
 
-mesh.add_attribute("face_centroid")                         # Compute centroids
-centroid = mesh.get_attribute("face_centroid")              # Retrieve them
+mesh.add_attribute("face_centroid")                                 # Compute centroids
+centroid = mesh.get_attribute("face_centroid")                      # Retrieve them
 
-mesh.add_attribute("vertex_normal")                         # Compute centroids
-v_normal = mesh.get_attribute("vertex_normal")              # Retrieve them
+normals     = normals.reshape(len(index), 3)                        # Reshape into triplets
+centroid    = centroid.reshape(len(index), 3)                       # Reshape into triplets
 
-mesh.add_attribute("vertex_index")                         # Compute centroids
-v_index = mesh.get_attribute("vertex_index")              # Retrieve them
+SCALE_FACTOR    = 1                                                 # Number of steps to slice the array into
+normals         = normals[::SCALE_FACTOR]                           # Slice the array (to reduce the number of points)
+centroid        = centroid[::SCALE_FACTOR]                          # ...so things go faster
 
-normals     = normals.reshape(len(index), 3)                # Reshape into triplets
-centroid    = centroid.reshape(len(index), 3)               # Reshape into triplets
-v_normal    = v_normal.reshape(len(v_index), 3)
+'''
+NOTE:-
+    np.where() returns the index of any element that satisfies
+    any part of the condition specified. i.e:
+    
+    >>> a = np.array( ([0,1,5], [3,2,1], [0,0,-1]) )
+    >>> a
+    array([[ 0,  1,  5],
+           [ 3,  2, -1],
+           [ 0,  0, -1]])
+           
+    >>> rigid_boundary  = np.array( np.where(a==(0, 0, -1)) ).T
+    >>> rigid_boundary
+    array([[0, 0],
+           [1, 2],
+           [2, 0],
+           [2, 1],
+           [2, 2]])
 
-SCALE_FACTOR    = 1
-normals         = normals[::SCALE_FACTOR]
-centroid        = centroid[::SCALE_FACTOR]
-rigid_boundary  = np.array( np.where(normals==(0, 0, -1)) ).T
-bound           = rigid_boundary[:,0]
-return_val      = rigid_boundary[:,1]
+    This is why we still do some more processing right after that
+'''
+rigid_boundary  = np.array( np.where(normals==(0, 0, -1)) ).T       # Search the normals for the condition (x, y, z)=(0, 0, -1)
+bound           = rigid_boundary[:,0]                               # Extract the bound indices
+return_val      = rigid_boundary[:,1]                               # Extract the 
+
+# Find arrays where ALL elements satisfy the condition specified
 index = list()
 for i in range( 0, rigid_boundary.shape[0], 3 ):
     if( return_val[i] == 0 and return_val[i+1] == 1 and return_val[i+2] == 2 ):
@@ -44,29 +79,31 @@ bound = np.zeros( len(index) )
 for i in range( 0, len(index) ):
     bound[i] = index[i]
         
-##rigid_boundary  = rigid_boundary[:,0][::3]                  # Extract indices
-##
-### Make the grid plane
-##x = centroid[:,0]; xx = np.copy( x )
-##y = centroid[:,1]; yy = np.copy( y )
-##z = centroid[:,2]; zz = np.copy( z )
-##
-### Make the direction data for the arrows
-##u = normals[:,0]; uu = np.copy( u )
-##v = normals[:,1]; vv = np.copy( v )
-##w = normals[:,2]; ww = np.copy( w )
-##
-##x, y, z = np.zeros_like(rigid_boundary), np.zeros_like(rigid_boundary), np.zeros_like(rigid_boundary)
-##u, v, w = np.zeros_like(rigid_boundary), np.zeros_like(rigid_boundary), np.zeros_like(rigid_boundary)
-##i = 0
-##for ndx in rigid_boundary:
-##    x[i], y[i], z[i] = xx[ndx], yy[ndx], zz[ndx]
-##    u[i], v[i], w[i] = uu[ndx], vv[ndx], ww[ndx]
-##    i += 1
-##
-##fig = plt.figure()
-##ax = fig.gca(projection='3d')
-##
-####ax.quiver(x, y, z, u, v, w, length=0.1, normalize=True)
-##ax.plot(x, y, z, 'o', markersize=10, color='g', alpha=0.2)
-##plt.show()
+rigid_boundary  = rigid_boundary[:,0][::3]                          # Extract indices
+
+# Make the grid plane
+x = centroid[:,0]; xx = np.copy( x )
+y = centroid[:,1]; yy = np.copy( y )
+z = centroid[:,2]; zz = np.copy( z )
+
+# Make the direction data for the arrows
+u = normals[:,0]; uu = np.copy( u )
+v = normals[:,1]; vv = np.copy( v )
+w = normals[:,2]; ww = np.copy( w )
+
+x, y, z = list(), list(), list()
+u, v, w = list(), list(), list()
+
+# Now populate the x, y, z points and the u, v, w normal vectors
+for ndx in rigid_boundary:
+    x.append(xx[ndx]); y.append(yy[ndx]); z.append(zz[ndx])
+    u.append(uu[ndx]); v.append(vv[ndx]); w.append(ww[ndx])
+
+# Finally plot
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+ax.quiver(x, y, z, u, v, w, length=0.1, normalize=True, alpha=0.25)
+ax.plot(x, y, z, 'o', markersize=10, color='g', alpha=0.75)
+plt.show()
+
