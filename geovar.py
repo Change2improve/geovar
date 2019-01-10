8af1f968c98551d9681dbe5b20060b998a9f2f20
NET\flobo <fluvio.lobo@gmail.com>
2019-01-10 17:32:12 -0500

    Started dividing main class in modules to delegate functionality.

--- geovar.py
+++ geovar.py
@@ -54,6 +54,9 @@ from    itertools                   import  product                     # Apply 
 import  numpy                       as      np                          # Fast array creation
 import  os, re                                                          # Dir/path manipulation, extract numerics from strings
 
+
+from    _setup                      import  *
+
 # ************************************************************************
 # =====================> CONSTRUCT ARGUMENT PARSER <=====================*
 # ************************************************************************
@@ -128,69 +131,71 @@ class geovar( object ):
         self.allow_export    = False                                    # Flag to allow STL exports
         self.valid_mutations = 0                                        # Counter for successful mutations
         
-        self.setup_directories()                                        # Setup & define directories
+        setup_directories( self )                                        # Setup & define directories
+        print( self.tet )
+        
         self.connect_to_sketch()                                        # Instantiate Onshape client and connect
 
         self.get_values( initRun=True )                                 # Get configurable part features and CURRENT default values
 
 # --------------------------
-
-    def setup_directories( self ):
-        '''
-        Create output folder and point to
-        location of compiled TetGen program
-        '''
-
-        # ------ UNIX systems ------
-        if( system()=='Linux' ):
-            src      = os.getcwd()
-            self.dst = "{}/output/{}/".format( src, datetime.now().strftime("%Y-%m-%d__%H_%M_%S") )
-            self.tet = args.tetgen_dir
-
-            try:
-                os.makedirs( self.dst )
-            except OSError:
-                print( "FAILED to create directory. Check permissions" )
-                quit()
-            else:
-                print( "Created {}".format(self.dst) )
-
-        # ----- Windows system -----
-        elif( system()=='Windows' ):
-            # Define useful paths
-            src      = os.getcwd()
-            self.dst = "{}\\output\\{}\\".format( src, datetime.now().strftime("%Y-%m-%d__%H_%M_%S") )
-            
-##            self.tet = args.tetgen_dir                                                  # Manual directory identification
-
-            # ------ Automated localization of tetgen directory ------ #
-            print( "BEGAN Automated localization of tetgen directory" )
-            dir_list = os.listdir()                                                     # List elements within current directory
-            dir_len  = len(dir_list)
-            test_string = 'tetgen'
-            test_string_len = len(test_string)
-            for i in range( 0, dir_len ):
-                if len( dir_list[i] ) > test_string_len:
-                    if dir_list[i][0:test_string_len] == test_string:
-                        print( "FOUND tetgen directory ...geovar\\" + dir_list[i] )
-                        match_index = i
-                        break
-
-            current_dir = os.getcwd()
-            tetgen_dir = current_dir + '\\' + dir_list[match_index] + '\\build\\Debug\\'
-            print( "CURRENT DIR: " + current_dir )
-            print( "TETGEN DIR: " + tetgen_dir )
-
-            self.tet = tetgen_dir                                                       # Passing tetgen path to the .self structure
-                
-
-            try:
-                os.makedirs( self.dst )
-            except WindowsError:
-                print( "FAILED to create directory. Check permissions" )
-                quit()
-            else:
-                print( "Created {}".format(self.dst) )
+##
+##    def setup_directories( self ):
+##        '''
+##        Create output folder and point to
+##        location of compiled TetGen program
+##        '''
+##
+##        # ------ UNIX systems ------
+##        if( system()=='Linux' ):
+##            src      = os.getcwd()
+##            self.dst = "{}/output/{}/".format( src, datetime.now().strftime("%Y-%m-%d__%H_%M_%S") )
+##            self.tet = args.tetgen_dir
+##
+##            try:
+##                os.makedirs( self.dst )
+##            except OSError:
+##                print( "FAILED to create directory. Check permissions" )
+##                quit()
+##            else:
+##                print( "Created {}".format(self.dst) )
+##
+##        # ----- Windows system -----
+##        elif( system()=='Windows' ):
+##            # Define useful paths
+##            src      = os.getcwd()
+##            self.dst = "{}\\output\\{}\\".format( src, datetime.now().strftime("%Y-%m-%d__%H_%M_%S") )
+##            
+####            self.tet = args.tetgen_dir                                                  # Manual directory identification
+##
+##            # ------ Automated localization of tetgen directory ------ #
+##            print( "BEGAN Automated localization of tetgen directory" )
+##            dir_list = os.listdir()                                                     # List elements within current directory
+##            dir_len  = len(dir_list)
+##            test_string = 'tetgen'
+##            test_string_len = len(test_string)
+##            for i in range( 0, dir_len ):
+##                if len( dir_list[i] ) > test_string_len:
+##                    if dir_list[i][0:test_string_len] == test_string:
+##                        print( "FOUND tetgen directory ...geovar\\" + dir_list[i] )
+##                        match_index = i
+##                        break
+##
+##            current_dir = os.getcwd()
+##            tetgen_dir = current_dir + '\\' + dir_list[match_index] + '\\build\\Debug\\'
+##            print( "CURRENT DIR: " + current_dir )
+##            print( "TETGEN DIR: " + tetgen_dir )
+##
+##            self.tet = tetgen_dir                                                       # Passing tetgen path to the .self structure
+##                
+##
+##            try:
+##                os.makedirs( self.dst )
+##            except WindowsError:
+##                print( "FAILED to create directory. Check permissions" )
+##                quit()
+##            else:
+##                print( "Created {}".format(self.dst) )
 
 # --------------------------
 
