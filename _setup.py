2e8afc626bcdfa1c2a526b68a10935ab1d200f61
NET\flobo <fluvio.lobo@gmail.com>
2019-01-15 15:00:57 -0500

    Seprarated the tetgen localization function from the setup_directories() functionb in the _setup module.

--- _setup.py
+++ _setup.py
@@ -16,14 +16,41 @@
 *
 '''
 
-from    platform                    import  system                      # Running platform info
-import  os, re                                                          # Dir/path manipulation, extract numerics from strings
-from    datetime                    import  datetime                    # Get date and time
+from    platform                    import  system                                  # Running platform info
+import  os, re                                                                      # Dir/path manipulation, extract numerics from strings
+from    datetime                    import  datetime                                # Get date and time
 
 
 # ************************************************************************
 # FUNCTIONS =============================================================*
 # ************************************************************************
+
+def setup_tetgen_directory( self ):
+    '''
+    Locates the path to the tetgen application within;
+        - The repository structure
+        - On a Windows OS
+    '''
+    print( "SETUP TETGEN DIR..." )
+    dir_list = os.listdir()                                                         # List elements within current directory
+    dir_len  = len(dir_list)
+    test_string = 'tetgen'
+    test_string_len = len(test_string)
+    for i in range( 0, dir_len ):
+        if len( dir_list[i] ) > test_string_len:
+            if dir_list[i][0:test_string_len] == test_string:
+                print( ">> FOUND tetgen directory ...geovar\\" + dir_list[i] )
+                match_index = i
+                break
+
+    current_dir = os.getcwd()
+    tetgen_dir = current_dir + '\\' + dir_list[match_index] + '\\build\\Debug\\'
+    print( ">> CURRENT DIR: " + current_dir )
+    print( ">> TETGEN DIR: " + tetgen_dir )
+
+    self.tet = tetgen_dir                                                           # Passing tetgen path to the .self structure
+           
+# ------------------------------------------------------------------------
 
 def setup_directories( self ):
     '''
@@ -51,27 +78,29 @@ def setup_directories( self ):
         src      = os.getcwd()
         self.dst = "{}\\output\\{}\\".format( src, datetime.now().strftime("%Y-%m-%d__%H_%M_%S") )
         
+        setup_tetgen_directory( self )
+        
 ##            self.tet = args.tetgen_dir                                                  # Manual directory identification
 
-        # ------ Automated localization of tetgen directory ------ #
-        print( "BEGAN Automated localization of tetgen directory" )
-        dir_list = os.listdir()                                                     # List elements within current directory
-        dir_len  = len(dir_list)
-        test_string = 'tetgen'
-        test_string_len = len(test_string)
-        for i in range( 0, dir_len ):
-            if len( dir_list[i] ) > test_string_len:
-                if dir_list[i][0:test_string_len] == test_string:
-                    print( "FOUND tetgen directory ...geovar\\" + dir_list[i] )
-                    match_index = i
-                    break
-
-        current_dir = os.getcwd()
-        tetgen_dir = current_dir + '\\' + dir_list[match_index] + '\\build\\Debug\\'
-        print( "CURRENT DIR: " + current_dir )
-        print( "TETGEN DIR: " + tetgen_dir )
-
-        self.tet = tetgen_dir                                                       # Passing tetgen path to the .self structure
+##        # ------ Automated localization of tetgen directory ------ #
+##        print( "BEGAN Automated localization of tetgen directory" )
+##        dir_list = os.listdir()                                                     # List elements within current directory
+##        dir_len  = len(dir_list)
+##        test_string = 'tetgen'
+##        test_string_len = len(test_string)
+##        for i in range( 0, dir_len ):
+##            if len( dir_list[i] ) > test_string_len:
+##                if dir_list[i][0:test_string_len] == test_string:
+##                    print( "FOUND tetgen directory ...geovar\\" + dir_list[i] )
+##                    match_index = i
+##                    break
+##
+##        current_dir = os.getcwd()
+##        tetgen_dir = current_dir + '\\' + dir_list[match_index] + '\\build\\Debug\\'
+##        print( "CURRENT DIR: " + current_dir )
+##        print( "TETGEN DIR: " + tetgen_dir )
+##
+##        self.tet = tetgen_dir                                                       # Passing tetgen path to the .self structure
             
 
         try:
