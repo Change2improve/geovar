383b1b6cd08d39af9b56c96c0d2c9a6aee94911c
Fluvio Lobo <fluvio.lobo@gmail.com>
2019-05-20 22:04:14 -0400

    Working on the formatting of the morph function/module

--- _morph.py
+++ _morph.py
@@ -116,9 +116,6 @@ def morph_geometry( self ):
 
     vals_range = list( range( arr.shape[1] ) )
     prods = list( product( vals_range, repeat = arr.shape[0] ) )
-    print( len(prods) )
-    print( prods )
-    print( prods[10][2] )
     
     param_prvs  = np.copy( arr[:,0] )                               # Previous unchanged value of the parameters
     print( param_prvs )
@@ -134,8 +131,7 @@ def morph_geometry( self ):
     
     # ------ Mutate  Part ------
     self.i = 0
-    for i in range( 0, 3 ):                                # Loop over ALL possible combinations
-        print( prods[i] )
+    for i in range( 0, 3 ):                                         # Loop over ALL possible combinations
         #print( fmt_str )                                            #   [INFO] Print FORMATTED key names
         #print( "=" * self.len_cte )                                 #   [INFO] Print adaptive width dashes
         #print( "{:8}:".format("SENT"), end='\t' )                   #   [INFO] Print values
@@ -144,8 +140,14 @@ def morph_geometry( self ):
         start   = time()                                            #   Timer for regeneration time
 
         for j in range( 0, arr.shape[0] ):                          #   Loop over ALL features
-            param_crnt[j] = arr[j][prods[i][j]]                       #       Get current value to be passed
+            param_crnt[j] = arr[j][prods[i][j]]                     #       Get current value to be passed
 
+            self.myPart.params = { self.keys[j]:                    #           Pass new value (aka mutate part)
+                                   param_crnt[j]*u.mm }             #           ...
+
+            param_prvs[j] = param_crnt[j]                           #           Update previous parameter
+            self.i += 1
+            
             '''
             if( param_crnt[j] != param_prvs[j] ):                   #       If current and previous parameters are different
                 self.myPart.params = { self.keys[j]:                #           Pass new value (aka mutate part)
