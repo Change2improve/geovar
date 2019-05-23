1f291522401abb98fb816548bd35980005305f85
NET\flobo <fluvio.lobo@gmail.com>
2019-05-23 16:46:51 -0400

    Added comments

--- _setup.py
+++ _setup.py
@@ -234,23 +234,18 @@ def generate_variant_array( self ):
 # ------------------------------------------------------------------------
 
 def query_variants(question, default="yes"):
-    """Ask a yes/no question via raw_input() and return their answer.
-
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
-
-    The "answer" return value is True for "yes" or False for "no".
     """
-    valid = {"yes": True, "y": True, "ye": True,
-             "no": False, "n": False}
+    STOP:
+        - Halts process until user reviews information
+    """
+    valid           = {"yes": True, "y": True, "ye": True,
+                       "no": False, "n": False}
     if default is None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
+        prompt      = " [y/n] "
+    elif default    == "yes":
+        prompt      = " [Y/n] "
+    elif default    == "no":
+        prompt      = " [y/N] "
     else:
         raise ValueError("invalid default answer: '%s'" % default)
 
