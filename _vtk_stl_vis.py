f654423966835c9c1ce6f663f4c2d0538197017a
Fluvio Lobo <fluvio.lobo@gmail.com>
2019-05-26 19:08:54 -0400

    Made some progress in the rendering of the .STL files. Also added a section to generate a screenshot of the window. The overall issue is the limited documentation.

--- _vtk_stl_vis.py
+++ _vtk_stl_vis.py
@@ -1,15 +1,22 @@
 import vtk
 from vtk.util.misc import vtkGetDataRoot
+from vtk.util.colors import *
 VTK_DATA_ROOT = vtkGetDataRoot()
 
 sr = vtk.vtkSTLReader()
-sr.SetFileName("C:/Users/WOLF512/Documents/Gits/PD3D/geovar/output/2019-05-26__17_29_33/Part_1_var3.stl")
+sr.SetFileName("C:/Users/WOLF512/Documents/Gits/PD3D/geovar/output/2019-05-26__17_29_33/Part_1_var1.stl")
 
 stlMapper = vtk.vtkPolyDataMapper()
 stlMapper.SetInputConnection(sr.GetOutputPort())
 
 stlActor = vtk.vtkLODActor()
 stlActor.SetMapper(stlMapper)
+stlActor.RotateX(45.0)
+stlActor.RotateY(45.0)
+stlActor.RotateZ(45.0)
+
+stlActor.GetProperty().SetColor(light_grey)
+
 # Create the Renderer, RenderWindow, and RenderWindowInteractor
 ren = vtk.vtkRenderer()
 renWin = vtk.vtkRenderWindow()
@@ -19,7 +26,7 @@ iren.SetRenderWindow(renWin)
 
 # Add the actors to the render; set the background and size
 ren.AddActor(stlActor)
-ren.SetBackground(0.1, 0.2, 0.4)
+ren.SetBackground(0.1, 0.6, 0.4)
 renWin.SetSize(500, 500)
  
 # Zoom in closer
@@ -27,6 +34,18 @@ ren.ResetCamera()
 cam1 = ren.GetActiveCamera()
 cam1.Zoom(1.4)
  
-iren.Initialize()
+#iren.Initialize()
 renWin.Render()
-iren.Start()
+
+# screenshot section
+w2if = vtk.vtkWindowToImageFilter()
+w2if.SetInput(renWin)
+w2if.Update()
+
+writer = vtk.vtkPNGWriter()
+writer.SetFileName("screenshot.png")
+writer.SetInputDataObject(w2if.GetOutput())
+writer.Write()
+
+
+#iren.Start()
