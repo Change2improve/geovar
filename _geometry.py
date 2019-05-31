dadd154ac7f0736437847174abdb07e26f889907
NET\flobo <fluvio.lobo@gmail.com>
2019-05-31 12:52:19 -0400

    Created a new module focused on the handling and operations of the geometries

---
+++ _geometry.py
@@ -0,0 +1,22 @@
+'''
+*
+* _geometry
+* GEOVAR GEOMETRY MODULE
+*
+* Module designed to delegate "geometry-specific" functions or operations
+* Geometries may include .STLs, .VTKs, etc.
+*
+*
+* AUTHOR                    :   Fluvio L. Lobo Fenoglietto
+* DATE                      :   May 31st, 2019
+*
+'''
+
+import  numpy                       as      np
+from    lxml                        import  etree
+
+
+# ************************************************************************
+# FUNCTIONS =============================================================*
+# ************************************************************************
+
