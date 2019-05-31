7d01d742b96bb5e3283d469a8bfdf47855ccfebe
NET\flobo <fluvio.lobo@gmail.com>
2019-05-31 10:04:29 -0400

    Completed routines for node and element extraction.

--- _test_sim.py
+++ _test_sim.py
@@ -1,4 +1,5 @@
 
+import  numpy                       as      np
 from    lxml                        import  etree
 
 #file = "C:\\Users\\WOLF512\\Documents\\Gits\\PD3D\\geovar\\input\\dogbone.feb"
@@ -17,34 +18,66 @@ print(geo_index) # validation
 
 
 # extracting parts from the geometry section
-geo_ele = root[geo_index]
-geo_len = len( geo_ele )
+geo = root[geo_index]
+geo_len = len( geo )
 print( geo_len )
 
-nodes = []
-elements = []
 
-fdata                                       = {}
-fdata['baseline']                           = {}
-fdata['baseline']['geo']                    = {}
-fdata['baseline']['geo']['nodes']           = {}
-fdata['baseline']['geo']['nodes']['id']     = []
-fdata['baseline']['geo']['nodes']['text']   = []
+fdata                                           = {}
+fdata['baseline']                               = {}
+fdata['baseline']['geo']                        = {}
+fdata['baseline']['geo']['nodes']               = {}
+fdata['baseline']['geo']['nodes']['id']         = []
+fdata['baseline']['geo']['nodes']['text']       = []
+fdata['baseline']['geo']['elements']            = {}
+fdata['baseline']['geo']['elements']['id']      = []
+fdata['baseline']['geo']['elements']['text']    = []
 
 for i in range( 0, geo_len ):
 
-    if geo_ele[i].tag == 'Nodes':
+    if geo[i].tag == 'Nodes': # -------------------------------------------------------------------------- #
 
-        nodes_ele = geo_ele[i]
-        nodes_len = len( nodes_ele )
+        nodes               = geo[i]
+        nodes_len           = len( nodes )
 
-        for j in range( 0, nodes_len ):
+        # initializing numpy arrays
+        nodes_array         = np.zeros(( nodes_len, 3 ), dtype=float)
+        
+        for j in range( 0, nodes_len ):           
+
+            # extracting raw data
+            ## extracting nodes
+            fdata['baseline']['geo']['nodes']['id'].append(         nodes[j].attrib['id'] )
+            fdata['baseline']['geo']['nodes']['text'].append(       nodes[j].text )
             
+            # tranform into numeric values for proper manipulation
+            nodes_array[j]  = nodes[j].text.split(',')
 
-            fdata['baseline']['geo']['nodes']['id'].append( nodes_ele[j].attrib['id'] )
-            fdata['baseline']['geo']['nodes']['text'].append( nodes_ele[j].text )
+    if geo[i].tag == 'Elements': # ----------------------------------------------------------------------- #
+
+        elements            = geo[i]
+        elements_len        = len( elements )
+        print( elements_len )
+
+        # initializing numpy arrays
+        elements_array      = np.zeros(( elements_len, 4 ), dtype=int)
+        
+        for j in range( 0, elements_len ):           
+
+            # extracting raw data
+            ## extracting elements
+            fdata['baseline']['geo']['elements']['id'].append(      elements[j].attrib['id'] )
+            fdata['baseline']['geo']['elements']['text'].append(    elements[j].text )
+            
+            # tranform into numeric values for proper manipulation
+            elements_array[j] = elements[j].text.split(',')
 
 
-backup = nodes_ele[0].text
-nodes_ele[0].text = " -1, -1, -1"
+# update structure
+fdata['baseline']['geo']['nodes']['array']          = nodes_array
+fdata['baseline']['geo']['elements']['array']       = elements_array
+
+
+backup = nodes[0].text
+nodes[0].text = " -1, -1, -1"
 _doc.write('dogbone_mod.xml')
