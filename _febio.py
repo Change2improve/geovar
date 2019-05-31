6759c11386db7a369bcb1350229b110db5d0e2f6
NET\flobo <fluvio.lobo@gmail.com>
2019-05-31 11:27:45 -0400

    Began structuring functions for better organization and efficiency.

--- _febio.py
+++ _febio.py
@@ -52,22 +52,96 @@ def read_febio_file( febio_filename ):
 # --------------------------
 
 
-def map_febio_components( febio_filename ):
+def get_febio_data( geo ):
     '''
-    Read FEBio file
+    Extract FEBio data from FEBio file:
+        - nodes
+        - elements
+        - nodesets
     '''
 
     print( '\n' )
-    print( "READ FEBio INFO..." )
-    #file = self.input + febio_filename
-    file = febio_filename
+    print( "EXTRACT FEBio INFO..." )
 
-    febio_doc       = etree.parse( file )
-    febio_root      = febio_doc.getroot()
+    # the inputs of this program should be the XML components of interest (e.g.: Geometry)
+    geo_len = len(geo)
 
-    return febio_doc, febio_root
-    #self.febio_doc  = febio_doc
-    #self.febio_root = febio_root
+    fdata                                           = {}
+    fdata['baseline']                               = {}
+    fdata['baseline']['geo']                        = {}
+    fdata['baseline']['geo']['nodes']               = {}
+    fdata['baseline']['geo']['nodes']['id']         = []
+    fdata['baseline']['geo']['nodes']['text']       = []
+    fdata['baseline']['geo']['elements']            = {}
+    fdata['baseline']['geo']['elements']['id']      = []
+    fdata['baseline']['geo']['elements']['text']    = []
+
+    for i in range( 0, geo_len ):
+
+        if geo[i].tag == 'Nodes': # -------------------------------------------------------------------------- #
+
+            fdata, nodes_array      = get_nodes(        geo, i, fdata )
+
+        if geo[i].tag == 'Elements': # ----------------------------------------------------------------------- #
+
+            fdata, elements_array   = get_elements(     geo, i, fdata )
+
+        if geo[i].tag == 'NoteSet': # ----------------------------------------------------------------------- #
+
+            fdata, elements_array   = get_elements(     geo, i, fdata )
+
+
+    # update structure
+    fdata['baseline']['geo']['nodes']['array']          = nodes_array
+    fdata['baseline']['geo']['elements']['array']       = elements_array
     
+    return fdata   
 
 # --------------------------
+
+def get_nodes( geo, index, fdata ):
+    '''
+    Get node data
+    '''
+    nodes               = geo[index]
+    nodes_len           = len( nodes )
+
+    # initializing numpy arrays
+    nodes_array         = np.zeros(( nodes_len, 3 ), dtype=float)
+    
+    for j in range( 0, nodes_len ):           
+
+        # extracting raw data
+        ## extracting nodes
+        fdata['baseline']['geo']['nodes']['id'].append(         nodes[j].attrib['id'] )
+        fdata['baseline']['geo']['nodes']['text'].append(       nodes[j].text )
+        
+        # tranform into numeric values for proper manipulation
+        nodes_array[j]  = nodes[j].text.split(',')
+
+    return fdata, nodes_array
+
+# --------------------------
+
+def get_elements( geo, index, fdata ):
+    '''
+    Get node data
+    '''
+    elements            = geo[index]
+    elements_len        = len( elements )
+    print( elements_len )
+
+    # initializing numpy arrays
+    elements_array      = np.zeros(( elements_len, 4 ), dtype=int)
+    
+    for j in range( 0, elements_len ):           
+
+        # extracting raw data
+        ## extracting elements
+        fdata['baseline']['geo']['elements']['id'].append(      elements[j].attrib['id'] )
+        fdata['baseline']['geo']['elements']['text'].append(    elements[j].text )
+        
+        # tranform into numeric values for proper manipulation
+        elements_array[j] = elements[j].text.split(',')
+
+    return fdata, elements_array
--- _test_sim.py
+++ _test_sim.py
@@ -7,63 +7,10 @@ file = 'dogbone.feb'
 
 febio_doc, root, geo, geo_index, geo_len = read_febio_file( file )
 
+fdata = get_febio_data( geo )
 
 
-fdata                                           = {}
-fdata['baseline']                               = {}
-fdata['baseline']['geo']                        = {}
-fdata['baseline']['geo']['nodes']               = {}
-fdata['baseline']['geo']['nodes']['id']         = []
-fdata['baseline']['geo']['nodes']['text']       = []
-fdata['baseline']['geo']['elements']            = {}
-fdata['baseline']['geo']['elements']['id']      = []
-fdata['baseline']['geo']['elements']['text']    = []
 
-for i in range( 0, geo_len ):
-
-    if geo[i].tag == 'Nodes': # -------------------------------------------------------------------------- #
-
-        nodes               = geo[i]
-        nodes_len           = len( nodes )
-
-        # initializing numpy arrays
-        nodes_array         = np.zeros(( nodes_len, 3 ), dtype=float)
-        
-        for j in range( 0, nodes_len ):           
-
-            # extracting raw data
-            ## extracting nodes
-            fdata['baseline']['geo']['nodes']['id'].append(         nodes[j].attrib['id'] )
-            fdata['baseline']['geo']['nodes']['text'].append(       nodes[j].text )
-            
-            # tranform into numeric values for proper manipulation
-            nodes_array[j]  = nodes[j].text.split(',')
-
-    if geo[i].tag == 'Elements': # ----------------------------------------------------------------------- #
-
-        elements            = geo[i]
-        elements_len        = len( elements )
-        print( elements_len )
-
-        # initializing numpy arrays
-        elements_array      = np.zeros(( elements_len, 4 ), dtype=int)
-        
-        for j in range( 0, elements_len ):           
-
-            # extracting raw data
-            ## extracting elements
-            fdata['baseline']['geo']['elements']['id'].append(      elements[j].attrib['id'] )
-            fdata['baseline']['geo']['elements']['text'].append(    elements[j].text )
-            
-            # tranform into numeric values for proper manipulation
-            elements_array[j] = elements[j].text.split(',')
-
-
-# update structure
-fdata['baseline']['geo']['nodes']['array']          = nodes_array
-fdata['baseline']['geo']['elements']['array']       = elements_array
-
-
-backup = nodes[0].text
-nodes[0].text = " -1, -1, -1"
-febio_doc.write('dogbone_mod.xml')
+#backup = nodes[0].text
+#nodes[0].text = " -1, -1, -1"
+#febio_doc.write('dogbone_mod.xml')
