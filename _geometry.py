25ed7f02974bd3dce7d0fc555d22d50b3872792e
Fluvio Lobo <fluvio.lobo@gmail.com>
2019-05-31 22:38:05 -0400

    Working on the first function of the geometry module, vtk reading

--- _geometry.py
+++ _geometry.py
@@ -13,10 +13,28 @@
 '''
 
 import  numpy                       as      np
+import  vtk
 from    lxml                        import  etree
+from    vtk.util.numpy_support      import  vtk_to_numpy
+
 
 
 # ************************************************************************
-# FUNCTIONS =============================================================*
+# FUNCTIONS ============================================================ *
 # ************************************************************************
+
+def read_vtk( filename ):
+
+    # load a vtk file as input
+    reader              = vtk.vtkUnstructuredGridReader()
+    reader.SetFileName("dogbone_var1.vtk")
+    reader.Update()
+
+    # Get the coordinates of nodes in the mesh
+    vtk_nodes           = reader.GetOutput().GetPoints().GetData()
+    vtk_nodes_array     = vtk_to_numpy( vtk_nodes )
+
+    return reader, vtk_nodes, vtk_nodes_array
+
+# --------------------------
 
